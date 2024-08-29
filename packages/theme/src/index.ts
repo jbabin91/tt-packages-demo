929ba642725a89161f4c54fb83d6ea0a62b32cac
Jace Babin <jbabin91@gmail.com>
2024-08-28 18:23:13 -0700

    fix(repo): :bug: fix ci script

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,34 +2,40 @@
 export { default as theme } from './lib/theme.js';
 
 // MUI Components
-// export type {
-//   AccordionActionsProps,
-//   AccordionDetailsProps,
-//   AccordionProps,
-//   AccordionSummaryProps,
-// } from './components/mui/accordion';
-// export {
-//   Accordion,
-//   AccordionActions,
-//   AccordionDetails,
-//   AccordionSummary,
-// } from './components/mui/accordion';
-// export type { AlertProps, AlertTitleProps } from './components/mui/alert';
-// export { Alert, AlertTitle } from './components/mui/alert';
-// export type { AppBarProps } from './components/mui/appBar';
-// export { AppBar } from './components/mui/appBar';
-// export type { AutocompleteProps } from './components/mui/autocomplete';
-// export { Autocomplete } from './components/mui/autocomplete';
-// export type { AvatarGroupProps, AvatarProps } from './components/mui/avatar';
-// export { Avatar, AvatarGroup } from './components/mui/avatar';
-// export type { BackdropProps } from './components/mui/backdrop';
-// export { Backdrop } from './components/mui/backdrop';
-// export type { BadgeProps } from './components/mui/badge';
-// export { Badge } from './components/mui/badge';
-// export type { BoxProps } from './components/mui/box';
-// export { Box } from './components/mui/box';
-// export type { BreadcrumbsProps } from './components/mui/breadcrumbs';
-// export { Breadcrumbs } from './components/mui/breadcrumbs';
+export type {
+  AccordionActionsProps,
+  AccordionDetailsProps,
+  AccordionProps,
+  AccordionSummaryProps,
+} from './components/mui/accordion/index.js';
+export {
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary,
+} from './components/mui/accordion/index.js';
+export type {
+  AlertProps,
+  AlertTitleProps,
+} from './components/mui/alert/index.js';
+export { Alert, AlertTitle } from './components/mui/alert/index.js';
+export type { AppBarProps } from './components/mui/appBar/index.js';
+export { AppBar } from './components/mui/appBar/index.js';
+export type { AutocompleteProps } from './components/mui/autocomplete/index.js';
+export { Autocomplete } from './components/mui/autocomplete/index.js';
+export type {
+  AvatarGroupProps,
+  AvatarProps,
+} from './components/mui/avatar/index.js';
+export { Avatar, AvatarGroup } from './components/mui/avatar/index.js';
+export type { BackdropProps } from './components/mui/backdrop/index.js';
+export { Backdrop } from './components/mui/backdrop/index.js';
+export type { BadgeProps } from './components/mui/badge/index.js';
+export { Badge } from './components/mui/badge/index.js';
+export type { BoxProps } from './components/mui/box/index.js';
+export { Box } from './components/mui/box/index.js';
+export type { BreadcrumbsProps } from './components/mui/breadcrumbs/index.js';
+export { Breadcrumbs } from './components/mui/breadcrumbs/index.js';
 export type {
   ButtonGroupProps,
   ButtonProps,
